6f1d4f5e11a5427f5ae28053c4469b4c423d9e10
Mustafa Demirci <demirci.msait@gmail.com>
2024-02-23 18:39:07 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='solidity-auto-compiler',
+    version='0.1',
+    description='Compile your .sol file when modified automatically by giving file path',
+    url='http://github.com/mustafa-demirci/solidity-auto-compiler',
+    author='Mustafa Demirci',
+    license='GNU',
+    py_modules=['md-solidity-auto-compiler']
+)
