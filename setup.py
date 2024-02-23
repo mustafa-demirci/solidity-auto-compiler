6b0a508f54860a197e93ebf7b184178b3ecf9a3b
Mustafa Demirci <demirci.msait@gmail.com>
2024-02-23 18:44:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,17 @@ setup(
     description='Compile your .sol file when modified automatically by giving file path',
     url='http://github.com/mustafa-demirci/solidity-auto-compiler',
     author='Mustafa Demirci',
+    author_email='your-email@example.com',  # Eğer paylaşmak istiyorsanız
     license='GNU',
-    py_modules=['md-solidity-auto-compiler']
+    packages=find_packages(),  # Modül yerine paket kullanıldığı için
+    install_requires=[  # Bağımlılıklarınızı burada belirtin
+        'py-solc-x',
+        'watchdog'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
